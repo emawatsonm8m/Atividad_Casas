c8201c8ab0d1c141eb9eee820bb10bef565f4871
Miyoshi <321270166@alumno.enp.unam.mx>
2023-06-11 16:05:31 -0600

    <miyoshi><fix></>:the files structure

---
+++ dynamics/php/login.php
@@ -0,0 +1,15 @@
+<!--HTML DOCUMENT-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--CSS-->
+    <link rel="stylesheet" href="../../libs/bootstrap/css/bootstrap.css">
+    <!--META-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include '../../templates/login.html'?>
+</body>
+</html>
